f2921a6b7a4db79512e5b6e699a37395e9531715
Dalton Pierce <dnnp2011@gmail.com>
2019-03-05 23:35:48 -0500

    Removed changelog.md - Removed unused logos - Added minified stock photos for Projects page - Implemented 2nd section of Projects page - Broke down Project page Sass into 7-1 Architecture for modularity - Added devicons dependency

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -1,13 +1,19 @@
-import { Grid, Paper, withWidth, Button } from '@material-ui/core';
+import { Grid, Paper, withWidth, Button, Typography } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { compose } from 'recompose';
-import whiteLogo from '../../assets/images/logo-terminal/white_logo_transparent_no_text.png';
+import { FaNode, FaSass, FaHtml5, FaCss3, FaCss3Alt, FaPython, FaGit } from 'react-icons/fa';
+import { IoLogoJavascript, IoLogoHtml5, IoLogoCss3, IoMdGitBranch } from 'react-icons/io';
+import classNames from 'classnames';
 import scss from './projects.module.scss';
 import themeStyles from './projects.style';
-import classNames from 'classnames';
+import whiteLogo from '../../assets/images/logo-terminal/white_logo_transparent_no_text_minified.png';
+import devStock1 from '../../assets/images/stock/web-dev-stock-800x533.jpg';
+import devStock2 from '../../assets/images/stock/web-dev-stock-3-800x533.jpg';
+import devStock3 from '../../assets/images/stock/web-dev-stock-4-800x533.jpg';
+import 'devicon';
 
 const Projects = props => {
   const { classes, width } = props;
@@ -18,7 +24,7 @@ const Projects = props => {
   return (
     <div className={scss.root}>
       <Paper elevation={5} className={scss.panel}>
-        <Grid container direction={'column'}>
+        <Grid wrap={'nowrap'} container direction={'column'}>
           {/* Header */}
           <Grid item className={scss.header}>
             <Grid
@@ -35,7 +41,6 @@ const Projects = props => {
                   aria-label={'SleeplessDev Logo'}
                   alt={'SleeplessDev Logo'}
                   src={whiteLogo}
-                  height={100}
                   className={scss.header__logo}
                 />
               </Grid>
@@ -46,12 +51,8 @@ const Projects = props => {
                 justify={'center'}
                 spacing={8}
                 alignItems={'center'}>
-                {/*    <Typography color={'inherit'} variant={'h1'} component={'h1'} gutterBottom noWrap align={'center'} autoCapitalize={'true'}>
-                 <span className={scss.headerTitle}>SleeplessDev</span><br />
-                 <span className={scss.headerSubtitle}>Making sure your website, is woke af</span>
-                 </Typography>*/}
                 <Grid item>
-                  <Grid container direction={'column'} className={scss['header__text-container']}>
+                  <Grid container direction={'column'} spacing={16} className={scss['header__text-container']}>
                     <h1 className={scss['heading-primary']}>
                       <span className={scss['heading-primary__title']}>SleeplessDev</span>
                       <span className={scss['heading-primary__subtitle']}>Wake up to the possibilities</span>
@@ -60,13 +61,163 @@ const Projects = props => {
                       className={classNames(scss.btn, scss['heading-primary__btn'], scss['btn--grow-in'])}
                       href={'#'}
                       color={'inherit'}
-                      aria-label={'jump to content'}>
-                      Discover
+                      aria-label={'scroll to projects'}>
+                      Projects
                     </a>
                   </Grid>
                 </Grid>
               </Grid>
             </Grid>
+          </Grid>
+
+          {/* About Brief */}
+          <Grid item className={scss.content}>
+            <main>
+              <section className={scss['section-about']}>
+                <Grid
+                  container
+                  direction={'column'}
+                  justify={'center'}
+                  alignContent={'center'}
+                  spacing={16}
+                  alignItems={'center'}>
+                  <Grid item>
+                    <Typography
+                      font={'inherit'}
+                      color={'inherit'}
+                      variant={'h2'}
+                      component={'h2'}
+                      className={scss['heading-secondary']}
+                      gutterBottom>
+                      A bit about me
+                    </Typography>
+                  </Grid>
+
+                  <Grid item>
+                    <Grid
+                      container
+                      direction={width === 'xs' ? 'column' : 'row'}
+                      spacing={width === 'xs' ? 32 : 0}
+                      justify={'space-between'}>
+                      <Grid item sm>
+                        <div className={scss['paragraph__container']}>
+                          <Grid container direction={'column'} spacing={16}>
+                            <Grid item>
+                              <Typography
+                                font={'inherit'}
+                                color={'inherit'}
+                                variant={'subtitle1'}
+                                component={'h3'}
+                                className={scss['heading-tertiary']}>
+                                Developer Journey
+                              </Typography>
+                              <Typography
+                                font={'inherit'}
+                                color={'inherit'}
+                                component={'p'}
+                                className={scss.paragraph}
+                                gutterBottom
+                                variant={'body1'}>
+                                I'm a self-taught full stack developer with a diverse range of skills revolving around
+                                computer software and hardware.I'm passionate about learning at every opportunity, as
+                                such, I've gained experience using many programming and scripting languages, frameworks,
+                                tools, and platforms.
+                              </Typography>
+                            </Grid>
+                            <Grid item>
+                              <Typography
+                                font={'inherit'}
+                                color={'inherit'}
+                                variant={'subtitle1'}
+                                component={'h3'}
+                                className={scss['heading-tertiary']}>
+                                Work I've Done
+                              </Typography>
+                              <Typography
+                                font={'inherit'}
+                                color={'inherit'}
+                                component={'p'}
+                                className={scss.paragraph}
+                                gutterBottom
+                                variant={'body1'}>
+                                I enjoy attending Meetups and conferences to learn about the latest technologies,
+                                network with fellow developers, and to extend my knowledge base both within and beyond
+                                tech.My desire and aptitude for learning new things quickly, as well as my
+                                fastidiousness and keen eye for details have been essential throughout the years of
+                                learning Computer Programming.Whatever I do, I do well and to the best of my ability.I'm
+                                confident that I will quickly become an asset to my new team, and look forward to
+                                learning new techniques, architectures, algorithms, and conventions.
+                              </Typography>
+                            </Grid>
+                            <Grid item>
+                              <a href={'#'} className={scss['btn-text']}>
+                                Go to resume &rarr;
+                              </a>
+                            </Grid>
+                          </Grid>
+                        </div>
+                      </Grid>
+                      <Grid item sm>
+                        <Grid
+                          container
+                          direction={width === 'xs' ? 'row' : 'column'}
+                          alignContent={'center'}
+                          justify={'center'}
+                          style={{ marginBottom: width === 'xs' ? '10rem' : '2rem' }}
+                          className={scss['composition']}>
+                          <img
+                            className={classNames(scss['composition__photo'], scss['composition__photo--p1'])}
+                            color={'inherit'}
+                            aria-label={'composition 1'}
+                            alt={'composition 1'}
+                            src={devStock1}
+                          />
+                          <img
+                            className={classNames(scss['composition__photo'], scss['composition__photo--p2'])}
+                            color={'inherit'}
+                            aria-label={'composition 3'}
+                            alt={'composition 3'}
+                            src={devStock3}
+                          />
+                          <img
+                            className={classNames(scss['composition__photo'], scss['composition__photo--p3'])}
+                            color={'inherit'}
+                            aria-label={'composition 2'}
+                            alt={'composition 2'}
+                            src={devStock2}
+                          />
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </section>
+
+              <section className={scss['section-skills']}>
+                <Grid container direction={'column'} spacing={0} alignContent={'center'} justify={'center'}>
+                  <Grid item>
+                    <Grid container direction={'row'} spacing={32} alignContent={'center'} justify={'space-evenly'}>
+                      <Grid item xs>
+                        <div className={scss['feature-box']}>
+                          <Typography
+                            className={classNames(scss['heading-tertiary'], scss.center)}
+                            variant={'h3'}
+                            component={'h3'}
+                            font={'inherit'}
+                            color={'inherit'}
+                            gutterBottom>
+                            Languages
+                          </Typography>
+                          <div className={'feature-box__icon-box'}>
+
+                          </div>
+                        </div>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </section>
+            </main>
           </Grid>
         </Grid>
       </Paper>
---
+++ src/devicons/devicons.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Devicons extends Component {
+  render() {
+    return (
+		<div>
+			placeholder
+		</div>
+		);
+  }
+}
