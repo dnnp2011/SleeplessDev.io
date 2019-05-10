2adb1b6943f5a99c0f2135005ec596b9fa8be1dc
Dalton Pierce <dnnp2011@gmail.com>
2019-05-09 23:14:46 -0400

    Removed Firebase config from gitignore - Added Procfile

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyBCAZbfWZW1cGv9Ybwf0xcSRCqXh9kLlVI",
+  authDomain: "sleeplessdev-io.firebaseapp.com",
+  databaseURL: "https://sleeplessdev-io.firebaseio.com",
+  projectId: "sleeplessdev-io",
+  storageBucket: "sleeplessdev-io.appspot.com",
+  messagingSenderId: "404197833424"
+};
+
+export default config;
